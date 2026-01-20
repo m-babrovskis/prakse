3198d0d4bf32bab9e64d892e63af03fd848e630d
Marks Babrovskis <m.babrovskis.dev@gmail.com>
2026-01-20 11:16:29 +0200

    a

--- index.php
+++ index.php
@@ -3,6 +3,5 @@ SESSION_START();
 $document_root = $_SERVER['DOCUMENT_ROOT']; //curent document root dir
 include_once $document_root . "/core/include.php";
 
-$router = new Router();
-$router->dispatch();
+include $document_root . "/theme/view/default/index.php";
 ?>
